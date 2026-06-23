de77105d4a556e1c9485e52581078a0ee347b20b
Abdul Rehman Galsulkar <abreh.galsulkar@gmail.com>
2026-06-22 23:12:28 -0400

    Add: move-zeroes - submission-3

---
+++ Data Structures & Algorithms/move-zeroes/submission-3.java
@@ -0,0 +1,29 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // left is the first 0
+        // right is the next non zeroe elemtn to swap with
+
+        int left = 0;
+        int right = 0;
+
+        while (left < nums.length-1 && nums[left] != 0) {
+            left++;
+        }
+        if (nums[left] != 0) {
+            return;
+        }
+
+        right = left + 1;
+
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                int temp = nums[right];
+                nums[right] = nums[left];
+                nums[left] = temp;
+
+                left++;
+            }
+            right++;
+        }
+    }
+}
